d4e9d117ced8d77199ec0b9c9699e15de8fe48e2
Jacob Shaw <JacobShawCode@gmail.com>
2018-10-19 18:52:47 -0500

    Week 6 Homework Done

---
+++ Week1/Week 6/MyTryCatchFinal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week1.Week1.Week_6
+{
+    public class MyTryCatchFinal
+    {
+        public static void CheckArrayList(ArrayList check)
+        {
+            try
+            {
+                Console.WriteLine("What is the name of the new Destination?");
+                string newDestination = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Add the new destination after which number?");
+                Console.WriteLine("Be careful to list a number, and not too high!");
+                string addDestination = Console.ReadLine();
+                int destinationNumber = Convert.ToInt32(addDestination);
+                Console.WriteLine();
+                check.Insert(destinationNumber, newDestination);
+                Console.WriteLine();
+
+                int counter = 1;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                foreach (Object destination in check)
+                {
+                    Console.WriteLine(counter + ". " + destination);
+                    counter++;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Oops! Sorry, our system is having problems with your response!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Your number ({0}) is out of range.",
+                    check.Count);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception thrown " + e.Message);
+            }
+            // The "finall" block of code will always execute.
+            finally
+            {
+                Console.WriteLine("We will now return you to the Main Menu.");
+            }
+        }
+    }
+}
--- Week1/Week6Homework.cs
+++ Week1/Week6Homework.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using Week1.Week1.Week_6;
+
 
 namespace ClassMaterial.Week1
 {
@@ -8,6 +11,69 @@ namespace ClassMaterial.Week1
     {
         public static void RunWeek6Homework()
         {
-        }   
+            Console.Clear();
+            SolarSystem();            
+
+            void SolarSystem()
+            {
+                ArrayList listSolarSystem = new ArrayList();
+                listSolarSystem.Add("Sun");
+                listSolarSystem.Add("Mercury");
+                listSolarSystem.Add("Venus");
+                listSolarSystem.Add("Earth");
+                listSolarSystem.Add("Mars");
+                listSolarSystem.Add("Ceres");
+                listSolarSystem.Add("Asteroid Belt");
+                listSolarSystem.Add("Jupiter");
+                listSolarSystem.Add("Saturn");
+                listSolarSystem.Add("Uranus");
+                listSolarSystem.Add("Neptune");
+                listSolarSystem.Add("Pluto");
+                listSolarSystem.Add("Haumea");
+                listSolarSystem.Add("Makemake");
+                listSolarSystem.Add("Kuiper Belt");
+                listSolarSystem.Add("Scattered Disk");
+                listSolarSystem.Add("Sedna");
+                listSolarSystem.Add("Eris");
+                listSolarSystem.Add("Planet X");
+                listSolarSystem.Add("Oort Cloud");
+
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("There are " + listSolarSystem.Count + " well known destinations in our Solar System.");
+                Console.ForegroundColor = ConsoleColor.Red;
+                int counter = 1;
+
+                foreach (Object destination in listSolarSystem)
+                {
+                    Console.WriteLine(counter + ". " + destination);
+                    counter++;
+                }
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Would you like to add one?");
+                Console.WriteLine("(Y)es  or  (N)o ?");
+
+                string userResponse = Console.ReadLine().ToUpper();
+                Console.WriteLine();
+
+                if (userResponse == "Y")
+                {
+                    MyTryCatchFinal.CheckArrayList(listSolarSystem);
+                }
+                else if (userResponse == "N")
+                {
+                    Console.WriteLine("Have a nice day!");
+                    Console.WriteLine("We will now return you to the Main Menu.");
+                }
+                else
+                {
+                    Console.WriteLine("I'll take that as a no, have a nice day!");
+                    Console.WriteLine("We will now return you to the Main Menu.");
+                }
+            }
+        }
     }
 }
+
